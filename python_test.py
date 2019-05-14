65e726d19bc6342acd851696dfa1899f9f9433ec
cupzy <33493174+cupzy@users.noreply.github.com>
2019-05-14 20:01:42 +0300

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,13 +1,13 @@
 class equation: 
 def __init__(self,a,b,c): 
-self.a = a; 
-self.b = b; 
-self.c = c; 
+  self.a = a; 
+  self.b = b; 
+  self.c = c; 
 def print(self): 
-print(self.a+'x^2 + '+self.b+'x + '+self.c+'=0'); 
+  print(self.a+'x^2 + '+self.b+'x + '+self.c+'=0'); 
 def solve(self): 
-d = int(self.b)**2 - 4*int(self.a)self.c; 
-return [((-int(self.b)+d**(1/2))/2/int(self.a)),((-int(self.b)-d**(1/2))/2/int(self.a))]; 
+  d = int(self.b)**2 - 4*int(self.a)self.c; 
+  return [((-int(self.b)+d**(1/2))/2/int(self.a)),((-int(self.b)-d**(1/2))/2/int(self.a))]; 
 inp = input('Enter a,b and c separated by whitespace\n'); 
 inp = inp.split(' '); 
 eq = equation(inp[0],inp[1],inp[2]); 
